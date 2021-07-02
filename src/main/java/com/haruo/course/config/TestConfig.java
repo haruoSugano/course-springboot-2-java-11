0f469b5a793ac134c16dd2f86a1a8f7034bf411d
Helio Haruo <haruosugano@icloud.com>
2021-07-01 21:17:13 -0300

    Payment, one-to-one association

--- src/main/java/com/haruo/course/config/TestConfig.java
+++ src/main/java/com/haruo/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.haruo.course.entities.Category;
 import com.haruo.course.entities.Order;
 import com.haruo.course.entities.OrderItem;
+import com.haruo.course.entities.Payment;
 import com.haruo.course.entities.Product;
 import com.haruo.course.entities.User;
 import com.haruo.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
